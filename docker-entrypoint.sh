85ec27359b7a32083f6c6bed3260fc1131f15709
dogwith1eye <dogwith1eye@gmail.com>
2019-02-19 13:31:38 -0500

    Initial Commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# replace cluster definition
+sed -i "s/AWS_ECS_CLUSTER_NAME/$AWS_ECS_CLUSTER_NAME/;s/AWS_DEFAULT_REGION/$AWS_DEFAULT_REGION/;s/AWS_VIRTUAL_HOST/$AWS_VIRTUAL_HOST/" /etc/traefik/traefik.toml
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- traefik "$@"
+fi
+
+# if our command is a valid Traefik subcommand, let's invoke it through Traefik instead
+# (this allows for "docker run traefik version", etc)
+if traefik "$1" --help 2>&1 >/dev/null | grep "help requested" > /dev/null 2>&1; then
+    set -- traefik "$@"
+fi
+
+exec "$@"
