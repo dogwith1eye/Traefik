814b43024ff2eb184307744fd865711aab3960ba
dogwith1eye <dogwith1eye@gmail.com>
2019-02-19 13:40:11 -0500

    Update deploy-envs.sh

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set environment variables used in deploy.sh and AWS task-definition.json:
-export IMAGE_NAME=jenkins-master
+export IMAGE_NAME=traefik
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
