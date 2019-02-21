2d2ea809239316d28ee42af6b9a443d5ea5945ef
dogwith1eye <dogwith1eye@gmail.com>
2019-02-21 14:22:22 -0500

    Moved in traefik files

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM      traefik:v1.7.9-alpine
-COPY      traefik_ecs.toml /etc/traefik/traefik.toml
-ENTRYPOINT ["traefik"]
--- build-windows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chmod +x ./traefik.toml
-dos2unix ./traefik.toml
-chmod +x ./deploy-envs.sh
-dos2unix ./deploy-envs.sh
-chmod +x ./scripts/build.sh
-dos2unix ./scripts/build.sh
-cd scripts && ./build.sh
